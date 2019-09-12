3cd8f19f0c5130a0700d0dcae5d900ea51c4be6c
King0625 <a7164265@gmail.com>
2019-09-12 14:54:21 +0800

    Change the mass assignment of User model

--- app/User.php
+++ app/User.php
@@ -31,7 +31,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'superuser', 'api_token'
+        'username', 'email', 'password', 'superuser', 'api_token'
     ];
 
     /**
