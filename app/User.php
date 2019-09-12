b98f381df63919f365c25dc814c06e4f5f0196eb
King0625 <a7164265@gmail.com>
2019-09-12 15:18:08 +0800

    Fix the error of relationship

--- app/User.php
+++ app/User.php
@@ -76,6 +76,6 @@ class User extends Authenticatable
     }
 
     public function profile(){
-        return $this->belongsTo(Profile::class);
+        return $this->hasOne(Profile::class);
     }
 }
